f4dc7e7f9ee130dd997a3abf7a7e55623299d7ec
Mehmet Tarhan <hi@memtarhan.com>
2023-04-24 22:46:25 +0100

    <dev>: create xcode project

---
+++ Moment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moment
+//
+//  Created by Mehmet Tarhan on 24/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
