e0ce934ec6507dfadd5ef424154c3167e70ad936
Mehmet Tarhan <hi@memtarhan.com>
2023-04-29 21:52:44 +0100

    <utils>: create SwiftUI project

---
+++ SwiftUI/Moment/Moment/MomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentApp.swift
+//  Moment
+//
+//  Created by Mehmet Tarhan on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
